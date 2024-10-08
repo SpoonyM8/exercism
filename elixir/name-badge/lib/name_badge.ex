c10966ad0cbc442c418d16d7d91f20118781d29a
Reece Withers <reece.withers2000@gmail.com>
2024-10-09 08:18:11 +1100

    Exercise 28 - name badge

---
+++ elixir/name-badge/lib/name_badge.ex
@@ -0,0 +1,10 @@
+defmodule NameBadge do
+  def print(id, name, department) do
+    base_str = "#{name} - #{String.upcase(get_department(department))}"
+    if is_nil(id), do: base_str, else: "[#{id}] - " <> base_str
+  end
+
+  defp get_department(department) do
+    if is_nil(department), do: "OWNER", else: String.upcase(department)
+  end
+end
