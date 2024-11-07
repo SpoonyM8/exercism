bb709c5caa8c9fb35a0b4fe785dda16491fe678d
Reece Withers <reece.withers2000@gmail.com>
2024-11-07 21:27:27 +1100

    Exercise 40 - primes

---
+++ elixir/prime-factors/lib/prime_factors.ex
@@ -0,0 +1,20 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest.
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(number), do: do_factors_for(number, 2, [])
+
+  defp do_factors_for(1, _, acc), do: acc
+  defp do_factors_for(number, divisor, acc) do
+    cond do
+      Kernel.rem(number, divisor) == 0 -> do_factors_for(Kernel.div(number, divisor), divisor, acc ++ [divisor])
+      true -> do_factors_for(number, divisor + 1, acc)
+    end
+  end
+end
